408f0caffb380943b5bfd3c999e7cfd4b9bc75df
elifboluk <elifbolukk@gmail.com>
2022-11-23 15:50:33 +0300

    Entity Eklendi

--- OtelMvcProject/Controllers/DefaultController.cs
+++ OtelMvcProject/Controllers/DefaultController.cs
@@ -9,31 +9,12 @@ namespace OtelMvcProject.Controllers
     public class DefaultController : Controller
     {
         // GET: Default
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult Index2()
-        {
-            return View();
-        }
-
-        public ActionResult AnaSayfa()
-        {
-            return View();
-        }
-
+      
         public ActionResult Hakkimizda()
         {
             return View();
         }
-
-        public ActionResult iletisim()
-        {
-            return View();
-        }
-
+        
         public PartialViewResult PartialFooter()
         {
             return PartialView();
