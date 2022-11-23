015d2a265cc25e71cebbbaeb67021fa5cbe1c283
elifboluk <elifbolukk@gmail.com>
2022-11-23 16:06:59 +0300

    Entity Eklendi

--- OtelMvcProject/Controllers/DefaultController.cs
+++ OtelMvcProject/Controllers/DefaultController.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using OtelMvcProject.Models.Entity;
 namespace OtelMvcProject.Controllers
 {
     public class DefaultController : Controller
     {
-        // GET: Default
+       DbOtelEntities db = new DbOtelEntities();
       
         public ActionResult Hakkimizda()
         {
-            return View();
+            var veriler = db.TblHakkimda.ToList();
+            return View(veriler);
         }
         
         public PartialViewResult PartialFooter()
