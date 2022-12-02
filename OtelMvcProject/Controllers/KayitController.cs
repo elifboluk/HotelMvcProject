219695e941b2b329d3997478cd00df7ba7d0a3b4
elifboluk <elifbolukk@gmail.com>
2022-12-03 00:21:07 +0300

    Kayıt Ol Sayfası

--- OtelMvcProject/Controllers/KayitController.cs
+++ OtelMvcProject/Controllers/KayitController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OtelMvcProject.Models.Entity;
 
 namespace OtelMvcProject.Controllers
 {
     public class KayitController : Controller
     {
-        // GET: Kayit
+        DbOtelEntities db = new DbOtelEntities();
+        
+        [HttpGet]
         public ActionResult KayitOl()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult KayitOl(TblYeniKayit p)
+        {
+            db.TblYeniKayit.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index","Login");
+            
         }
     }
 }
