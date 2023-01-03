11fee53a221932d5acd708e250d468a815c63670
elifboluk <elifbolukk@gmail.com>
2023-01-03 23:28:36 +0300

    Misafir Profilim Sayfası

--- OtelMvcProject/Controllers/MisafirController.cs
+++ OtelMvcProject/Controllers/MisafirController.cs
@@ -14,12 +14,14 @@ namespace OtelMvcProject.Controllers
         // GET: Misafir
         public ActionResult Index()
         {
-            return View();
+            var misafirmail = (string)Session["Mail"];
+            var misafirbilgi = db.TblYeniKayit.Where(x => x.Mail == misafirmail).ToList();
+            return View(misafirbilgi);
         }
 
         public ActionResult Rezervasyonlarim()
         {
-            var misafirmail = (string)Session["MusteriMail"];
+            var misafirmail = (string)Session["Mail"];
             ViewBag.a = misafirmail;
             var degerler = db.TblRezervasyon.Where(x => x.Misafir == 2).ToList();
             return View(degerler);
